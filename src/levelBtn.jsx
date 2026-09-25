89345bcbf1ae8825174da10d03cc3fe366923787
Alif <mdhba2023@gmail.com>
2026-09-25 21:53:19 +0600

    feat: enhance LevelBtn and Home component styling for improved user experience

--- src/levelBtn.jsx
+++ src/levelBtn.jsx
@@ -4,22 +4,20 @@ function LevelBtn({ level, subText }) {
     const navigate = useNavigate();
 
     return (
-        <>
-            <button className="h-48 w-80 rounded-3xl hover:shadow-2xl bg-custom-primary-hover hover:cursor-pointer hover:bg-custom-primary active:bg-custom-primary-dark" onClick={() => navigate({ to: `/jlpt-n${level}` })}>
-                <div className="flex justify-between px-10 pt-10 pb-5">
-                    <div className="text-left">
-                        <p className="text-3xl mb-2">N{level}</p>
-                        <p className="font-sans font-extralight">{subText}</p>
-                    </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
+        <button className="h-48 w-70 border border-custom-border-hover border-1 rounded-3xl hover:shadow-2xl bg-custom-secondary hover:cursor-pointer hover:bg-custom-secondary-hover active:bg-custom-secondary" onClick={() => navigate({ to: `/jlpt-n${level}` })}>
+            <div className="flex justify-between px-10 pt-10 pb-5">
+                <div className="text-left">
+                    <p className="text-3xl mb-2 text-custom-text">N{level}</p>
+                    <p className="font-sans font-extralight text-custom-text-muted">{subText}</p>
                 </div>
-                <hr className="" />
-                <div className="flex justify-between py-3 px-5">
-                    <p>Start leaning</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
-                </div>
-            </button>
-        </>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="text-custom-text-muted"><path d="M7 7h10v10" /><path d="M7 17 17 7" /></svg>
+            </div>
+            <hr className="border-custom-border-hover" />
+            <div className="flex justify-between py-3 px-5">
+                <p className="text-custom-text-muted">Start learning</p>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="text-custom-text-muted"><path d="M5 12h14" /><path d="m12 5 7 7-7 7" /></svg>
+            </div>
+        </button>
     )
 }
 
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -9,11 +9,11 @@ function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="w-full min-h-screen text-center bg-custom-background font-english">
+        <div className="w-full min-h-screen text-center bg-custom-background font-english text-custom-text">
             <div className="py-15">
-                <p>Your learning journey</p>
-                <p className="text-5xl my-2">Select your level</p>
-                <p className="pb-5">Choose a JLPT level and start learning at your own pace.</p>
+                <p className="mb-3 text-xs font-semibold uppercase text-custom-text-muted">Your Learning Journey</p>
+                <h1 className="mb-4 text-center text-5xl font-semibold">Select your level</h1>
+                <p className="mb-12 text-center text-sm text-custom-text-muted">Choose a JLPT level and start learning at your own pace.</p>
                 <div className="flex justify-center gap-5 flex-wrap m-auto mt-10 w-250 text-custom-mint-light">
                     <LevelBtn level={1} subText={"Advanced"} />
                     <LevelBtn level={2} subText={"Upper Intermediate"} />
